99b829f5423fc3d248f917cea43e9286b19c261a
Jeremy Laabs <jeremy_calcium@homtail.com>
2023-04-06 09:32:22 -0500

    Intro JS Problems Completed

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ Do the following:
 
    HINT: no function required
 */
-
+const votingAge = 18
+if(votingAge >= 18){
+  console.log('task 1 a:', true);
+}else{
+  console.log('task 1 a:', false);
+}
 
 
 /*
@@ -33,7 +38,13 @@ Do the following:
 
    HINT: no function required
 */
-
+let thingOne = "Glow"
+let thingTwo = "Key";
+if(thingTwo === "Glow"){
+  thingOne = "Shiny";
+} else {
+  console.log('task 1 b:', thingTwo)
+}
 
 
 
@@ -48,7 +59,8 @@ Do the following:
 
    HINT: look up the Number method
 */
-
+const year = '1999';
+console.log('task 1 c:', Number(year));
 
 
 
@@ -64,7 +76,7 @@ Do the following:
 function multiply(num1, num2){
   return num1 * num2;
 }
-
+console.log('task 1 d:', multiply(2,3));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -77,10 +89,10 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age1){
+  return age1 * 7;
 }
-
+console.log('task 2:', dogYears(32));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -101,7 +113,7 @@ Adult Dogs 1 year and older
    up to 5 lbs - 5% of their body weight
    6 - 10 lbs - 4% of their body weight 
    11 - 15 lbs - 3% of their body weight 
-   > 15lbs - 2% of their body weight 
+   > 15 lbs - 2% of their body weight 
 
 Puppies less than 1 year
    2 - 4 months 10% of their body weight
@@ -119,7 +131,7 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
             Received: undefined
 
               21 | describe('hungryDogFunction', ()=>{ 
-              22 |   it('Dog is 1 year and is 5lbs or less', ()=>{
+              22 |   it('Dog is 1 year and is 5 lbs or less', ()=>{
             > 23 |     expect(functions.hungryDog(4, 1)).toBe(0.2);
                 |
           ^
@@ -129,11 +141,26 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
         So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
+function hungryDog(weight, age){
+  if(age >= 1 && weight <= 5){
+    return weight * .05;
+  }else if(age >= 1 && weight >= 6 && weight <= 10){
+    return weight * .04;
+  }else if(age >= 1 && weight >= 11 && weight <= 15){
+    return weight * .03;
+  }else if(age >= 1 && weight > 15){
+    return weight * .02;
+  }else if(age < 1 && age >= .1667 && age <= .333){
+    return weight * .10; 
+  }else if(age < 1 && age >= .334 && age <= .583){
+    return weight * .05;
+  }else if(age >= .584 && age < 1){
+    return weight * .04;
+  }else{
+    return 'Enter new weight'
+  }
 }
-
-
+console.log('task 3:', hungryDog(15,1))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -155,11 +182,28 @@ Use the game function below to do the following:
 
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
 */
-
-function game(user, computer){
-  /*add your code here*/
+let computer = Math.random()
+if(computer <= .334){
+  computer = 'rock';
+}else if(computer <= .667){
+  computer = 'paper';
+}else if(computer >.667){
+  computer = 'scissors';
 }
-
+function game(user, computer){
+  if(user === computer){
+    return `it's a tie`;
+  }else if(user === 'rock' && computer === 'scissors'){
+    return 'you win!';
+  }else if(user === 'paper' && computer === 'rock'){
+        return 'you win!';
+  }else if(user === 'scissors' && computer === 'paper'){
+        return 'you win!';      
+  }else{
+    return `you lose!`
+  }
+}
+console.log('task 4:', game('rock', computer))
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -173,10 +217,10 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return km * .621371;
 }
-
+console.log('task 5a:', miles(2),'miles');
 
 
 //Task 5b - Centimeters to Feet
@@ -187,9 +231,10 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48
 }
+console.log('task 5b:', feet(200), 'feet');
 
 
 
@@ -207,10 +252,12 @@ Using the annoyingSong function below do the following:
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
-}
-
+function annoyingSong(number){
+      for(let i = number; i > 0; i --) {
+        return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i-1} bottles of soda on the wall`;
+      }
+ }
+console.log('task 6:', annoyingSong(5));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -227,10 +274,20 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(number){
+  if(number >= 90){
+    return 'you got a n A'
+  }else if(number < 90 && number >= 80){
+    return 'you got a B';
+  }else if(number < 80 && number >= 70){
+    return 'you got a C';
+  }else if(number < 70 && number >= 60){
+    return 'you got a D';
+  }else if(number < 60 && number >= 0){
+    return 'you got a F';
+  }
 }
-
+console.log('task 7', grade(79));
 
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
